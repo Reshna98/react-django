2e3a84909e6ec24faaaa6ce246b2578eea799dd9
unknown <reshnasuresh567@gmail.com>
2024-06-05 16:35:53 +0530

    2

--- Django/emppro/empapp/models.py
+++ Django/emppro/empapp/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Customuser(AbstractUser):
+    pass
+
 class Userdata(models.Model):
     first_name = models.CharField(max_length=100,null=True,blank=True) 
     last_name = models.CharField(max_length=100,null=True,blank=True) 
@@ -10,8 +15,7 @@ class Userdata(models.Model):
     user_type = models.CharField(max_length=100,null=True,blank=True) 
     course_completed=models.CharField(max_length=255,unique=True)
     department=models.CharField(max_length=255,unique=True)
-    email = models.CharField(max_length=100,null=True,blank=True) 
     address=models.TextField(blank=True,null=True)
     age=models.CharField(max_length=255,blank=True,null=True)
     certification=models.FileField(upload_to='docs/',null=True,blank=True)
-    about=models.TextField(blank=True,null=True)
+    
